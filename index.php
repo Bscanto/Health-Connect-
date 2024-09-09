827a430770d6d3a57589b28bdc9a018e450334f7
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-09 00:10:24 -0300

    area login

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,6 +12,13 @@
 </head>
 
 <body>
+  <div class="login">
+    <div class="form">
+      <form class="registro" action="">
+
+      </form>
+    </div>
+  </div>
 
 
 </body>
