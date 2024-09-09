762b62d188c9565011dbb8d107e1a4a96f3effca
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-08 23:50:36 -0300

    style

--- index.php
+++ index.php
@@ -4,11 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>Document</title>
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="shortcut icon" href="./img/logo vermelho.png" type="image/x-icon">
+
+  <title>Health-Connect</title>
 
 </head>
 
 <body>
-  
+
+
 </body>
 </html>
