5d3a428ba744aa756f2ac287aaba0a989332b735
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-12 16:03:07 -0300

    arquivo conexao

--- conexao.php
+++ conexao.php
@@ -1,5 +1,15 @@
 <?php 
 
+//CONEXÃO LOCAL COM O BANCO DE DADOS
+$servidor = 'localhost';
+$banco = 'health';
+$usuario = 'root';
+$senha = '';
+
+
+
+
+//VARIAB=VEIS GLOBAIS
 $nome_sistema = 'Health Connect';
 $nome = "Bruno";
 $email = "bsccanto@gmail.com";
