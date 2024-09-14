bb64a5cc6458051cd2d2110ab5bc874c72e73c35
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-13 23:15:38 -0300

    verificanto consulta

--- autenticar.php
+++ autenticar.php
@@ -1,11 +1,12 @@
 <?php 
 require_once("conexao.php");
 
-
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-$query = $pdo->query("SELECT * FROM usuarios");
-
+$query = $pdo->prepare("SELECT * from usuarios where email = '$usuario' and senha = '$senha'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = count($res);
+echo $linhas;
 
 ?>
