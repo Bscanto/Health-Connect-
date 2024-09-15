4ae368804d98e08cf20cd0389d824b085d4d2996
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-15 08:55:18 -0300

    Metodo if php redirecionamento de pagina

--- autenticar.php
+++ autenticar.php
@@ -4,9 +4,19 @@ require_once("conexao.php");
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-$query = $pdo->prepare("SELECT * from usuarios where email = '$usuario' and senha = '$senha'");
+$query = $pdo->prepare("SELECT * from usuarios where email = :email and senha = :senha");
+
+$query->bindValue(":email", "$usuario");
+$query->bindValue(":senha", "$senha");
+$query->execute();
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $linhas = count($res);
-echo $linhas;
+
+if($linhas > 0){
+
+}else{
+	echo '<script>window.alert("Dados Incorretos!!")</script>'; 
+	echo '<script>window.location="index.php"</script>';  
+}
 
 ?>
