f63f336b1d0d99da35a9d589f69e556567a0687b
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-15 21:46:39 -0300

    criando variaveis de sessão

--- autenticar.php
+++ autenticar.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+@session_start();
 require_once("conexao.php");
 
 $usuario = $_POST['usuario'];
@@ -10,10 +11,20 @@ $query->bindValue(":email", "$usuario");
 $query->bindValue(":senha", "$senha");
 $query->execute();
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
-$linhas = count($res);
+$linhas = @count($res);
 
 if($linhas > 0){
 
+	if($res[0]['ativo'] != 'Sim'){
+		echo '<script>window.alert("Seu acesso foi desativado!!")</script>'; 
+		echo '<script>window.location="index.php"</script>';  
+	}
+
+	$_SESSION['nome'] = $res[0]['nome'];
+	$_SESSION['id'] = $res[0]['id'];
+	$_SESSION['nivel'] = $res[0]['nivel'];
+
+	echo '<script>window.location="painel"</script>';
 }else{
 	echo '<script>window.alert("Dados Incorretos!!")</script>'; 
 	echo '<script>window.location="index.php"</script>';  
