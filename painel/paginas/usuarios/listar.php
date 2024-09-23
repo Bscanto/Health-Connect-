1488f490963f13db2c512c14f14666745b5ae7fb
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-23 12:53:39 -0300

    tabela usuarios

--- painel/paginas/usuarios/listar.php
+++ painel/paginas/usuarios/listar.php
@@ -1,3 +1,25 @@
 <?php 
-  echo "teste";
+$tabela = 'usuarios';
+require_once("../../../conexao.php");
+
+$query = $pdo->query("SELECT * from usuarios");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = @count($res);
+if($linhas > 0){
+  echo <<<HTML
+<small>
+	<table class="table table-hover" id="tabela">
+	<thead> 
+	<tr> 
+	<th>Nome</th>	
+	<th class="esc">Telefone</th>	
+	<th class="esc">Email</th>	
+	<th class="esc">Nível</th>	
+	<th>Ações</th>
+	</tr> 
+	</thead> 
+	<tbody>	
+HTML;
+
+}
 ?>
