893af4aa07997b61cd67165fb240665a784c3da8
Bruno Canto <bruno_scanto@hotmail.com>
2024-09-29 18:24:51 -0300

    Suas mensagens de commit aqui

--- autenticar.php
+++ autenticar.php
@@ -1,6 +1,7 @@
 <?php 
 @session_start();
 require_once("conexao.php");
+
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 $senha_crip = sha1($senha);
@@ -14,21 +15,32 @@ $linhas = @count($res);
 
 if($linhas > 0){
 
-	if($res[0]['ativo'] != 'Sim'){
-		echo '<script>window.alert("Seu acesso foi desativado!!")</script>'; 
-		echo '<script>window.location="index.php"</script>';
-	}
-	$_SESSION['nome'] = $res[0]['nome'];
-	$_SESSION['id'] = $res[0]['id'];
-	$_SESSION['nivel'] = $res[0]['nivel'];
+    if($res[0]['ativo'] != 'Sim'){
+        echo '<script>window.alert("Seu acesso foi desativado!!")</script>'; 
+        echo '<script>window.location="index.php"</script>';
+        exit();
+    }
 
-	
-	echo '<script>window.location="painel"</script>';
+    // Definir as variáveis de sessão
+    $_SESSION['nome'] = $res[0]['nome'];
+    $_SESSION['id'] = $res[0]['id'];
+    $_SESSION['nivel'] = $res[0]['nivel'];
 
-}else{
-	echo '<script>window.alert("Dados Incorretos!!")</script>'; 
-	echo '<script>window.location="index.php"</script>';  
+    // Verificar o nível de acesso e redirecionar
+    if($res[0]['nivel'] == 'Administrador' or $res[0]['nivel'] == 'Diretor'){
+        echo '<script>window.location="painel"</script>';
+
+    } elseif($res[0]['nivel'] == 'Profissional'){
+        echo '<script>window.location="painel-profissional"</script>';
+				
+    } else {
+        echo '<script>window.alert("Nível de acesso desconhecido!")</script>';
+        echo '<script>window.location="index.php"</script>';
+    }
+
+} else {
+    echo '<script>window.alert("Dados Incorretos!!")</script>'; 
+    echo '<script>window.location="index.php"</script>';  
 }
 
-
- ?>
+?>
